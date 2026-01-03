b7eac97efa867e3b79d1ee9a8160a0d2e8e46fbe
hanisa1 <hanisamohamed1@gmail.com>
2026-01-04 00:05:00 +1100

    Initial Commit

---
+++ Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by Hanisa Hilole on 3/1/2026.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Apple-Frameworks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Apple-Frameworks
+//
+//  Created by Hanisa Hilole on 3/1/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
